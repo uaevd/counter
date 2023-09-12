23c2f2f9921dfc9f41c935be48bc1889efcdb3be
uaevd <saekkigae45@gmail.com>
2023-09-12 20:28:22 +0700

    Apply styled in Counter

--- src/components/organisms/Counter/index.tsx
+++ src/components/organisms/Counter/index.tsx
@@ -1,10 +1,18 @@
+import styled from '@emotion/styled';
+
 import { Button } from 'components/atoms/Button';
 import { Count } from 'components/atoms/Count';
 
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
 export const Counter = () => (
-    <div>
+    <Container>
         <Button label="-" />
         <Count value={0} />
         <Button label="+" />
-    </div>
+    </Container>
 );
