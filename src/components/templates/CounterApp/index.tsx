263d323c8a3f6d391c2874456dd20a9540f2af10
uaevd <saekkigae45@gmail.com>
2023-09-12 20:35:03 +0700

    Add CounterApp

---
+++ src/components/templates/CounterApp/index.tsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+import { Title } from 'components/atoms/Title';
+import { Counter } from 'components/organisms/Counter';
+
+const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const CounterApp = () => (
+    <Container>
+        <Title title="Counter App" />
+        <Counter />
+    </Container>
+);
