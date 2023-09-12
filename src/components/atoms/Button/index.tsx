dca12e67c461512c94af76123f9a5b5fc6250716
uaevd <saekkigae45@gmail.com>
2023-09-12 20:11:11 +0700

    Add Props, Story args

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -18,4 +18,11 @@ const Container = styled.button`
     }
 `;
 
-export const Button = () => <Container>Button</Container>;
+interface Props {
+    readonly label: string;
+    readonly onClick?: () => void;
+}
+
+export const Button = ({ label, onClick }: Props) => (
+    <Container onClick={onClick}>{label}</Container>
+);
