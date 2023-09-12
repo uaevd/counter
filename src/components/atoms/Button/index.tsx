12a046a43e776c0a08e2c1da594b72cabd002036
uaevd <saekkigae45@gmail.com>
2023-09-12 20:07:25 +0700

    Apply styled

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,1 +1,21 @@
-export const Button = () => <button>Button</button>;
+import styled from '@emotion/styled';
+
+const Container = styled.button`
+    border: 0;
+    color: #fff;
+    background-color: #ff5722;
+    cursor: pointer;
+    padding: 8px 16px;
+    border-radius: 4px;
+
+    &:hover {
+        opacity: 0.8;
+    }
+
+    &:active {
+        opacity: 1;
+        box-shadow: inset 5px 5px 10px rgba(0, 0, 0, 0.2);
+    }
+`;
+
+export const Button = () => <Container>Button</Container>;
